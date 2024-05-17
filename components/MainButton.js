af269a8998200dbb12d081dc04f1f0e84c3902a3
mikewanjero <mikewanj@gmail.com>
2024-05-17 12:11:36 +0300

    Text Input and keyboard style changes

--- components/MainButton.js
+++ components/MainButton.js
@@ -1,9 +1,41 @@
-import { View, Text } from "react-native";
+import { View, Text, Pressable, StyleSheet } from "react-native";
 
 export default function MainButton({ children }) {
+  function PressHandler() {
+    console.log("Pressed!");
+  }
+
   return (
-    <View>
-      <Text>{children}</Text>
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        style={({ pressed }) =>
+          pressed
+            ? [styles.buttonInnerContainer, styles.pressed]
+            : styles.buttonInnerContainer
+        }
+        onPress={PressHandler}
+        android_ripple={{ color: "#5f0431" }}
+      >
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: "hidden",
+  },
+  buttonInnerContainer: {
+    backgroundColor: "#84164d",
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+    elevation: 2,
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+  },
+});
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -4,7 +4,13 @@ import MainButton from "../components/MainButton";
 export default function StartGameScreen() {
   return (
     <View style={Styles.inputContainer}>
-      <TextInput />
+      <TextInput
+        style={Styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
       <MainButton>RESET</MainButton>
       <MainButton>CONFIRM</MainButton>
     </View>
@@ -19,5 +25,16 @@ const Styles = StyleSheet.create({
     padding: 16,
     backgroundColor: "#72063c",
     elevation: 5,
+  },
+  numberInput: {
+    height: 50,
+    width: 75,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign: "center",
   },
 });

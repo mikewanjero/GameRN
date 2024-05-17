5da1a25cf85efcfa6a0a7992386fd7dd607ebe59
mikewanjero <mikewanj@gmail.com>
2024-05-17 16:54:18 +0300

    added default screen text

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,5 +1,7 @@
-import React from "react";
+import { Text, View, StyleSheet } from "react-native";
 
-export default function GameScreen() {}
+export default function StartGameScreen() {
+  return <Text>Game Screen!</Text>;
+}
 
 const Styles = StyleSheet.create({});

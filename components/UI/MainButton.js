4495e56428ec51358b6faf814c6c6ce16d2bd8e3
mikewanjero <mikewanj@gmail.com>
2024-05-21 16:39:53 +0300

    Added Main Button to the UI folder in components

---
+++ components/UI/MainButton.js
@@ -0,0 +1,43 @@
+import { View, Text, Pressable, StyleSheet } from "react-native";
+import { Colors } from "../../defaults/colors";
+
+export default function MainButton({ children, onButtonPress }) {
+  function PressHandler() {
+    onButtonPress();
+    console.log("Pressed!");
+  }
+
+  return (
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        style={({ pressed }) =>
+          pressed
+            ? [styles.buttonInnerContainer, styles.pressed]
+            : styles.buttonInnerContainer
+        }
+        onPress={PressHandler}
+        android_ripple={{ color: Colors.primary600 }}
+      >
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: "hidden",
+  },
+  buttonInnerContainer: {
+    backgroundColor: Colors.primary500,
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+    elevation: 2,
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+  },
+});

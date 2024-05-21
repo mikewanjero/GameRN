e9dc215f7be0e4900eb1fc2f43c2549b398bddb5
mikewanjero <mikewanj@gmail.com>
2024-05-21 16:40:46 +0300

    Added Number Container as a new UI element

---
+++ components/Game/NumberContainer.js
@@ -0,0 +1,27 @@
+import { View, Text, StyleSheet } from "react-native";
+import { Colors } from "../../defaults/colors";
+
+export default function NumberContainer({ children }) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.numberText}>{children}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    borderWidth: 4,
+    borderColor: Colors.primary800,
+    padding: 24,
+    margin: 24,
+    borderRadius: 8,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  numberText: {
+    color: Colors.primary800,
+    fontSize: 40,
+    fontWeight: "bold",
+  },
+});

8c77b9f5cc3b3f9964679cf0e6d8cc67bab60598
mikewanjero <mikewanj@gmail.com>
2024-05-23 14:10:19 +0300

    Added Game Over text and style

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,5 +1,17 @@
 import React from "react";
+import { Text, StyleSheet, SafeAreaView } from "react-native";
 
-export default function GameOverScreen() {}
+export default function GameOverScreen() {
+  return (
+    <SafeAreaView>
+      <Text style={styles.textView}>Game Over! Do you want to try again?</Text>
+    </SafeAreaView>
+  );
+}
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  textView: {
+    marginLeft: 20,
+    marginTop: 100,
+  },
+});
